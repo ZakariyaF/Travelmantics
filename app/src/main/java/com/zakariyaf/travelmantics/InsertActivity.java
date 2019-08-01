a05b4582cf6445e040732a4b1b43173fb69098a1
ZakariyaF <zak.ferhi@gmail.com>
2019-08-01 14:57:16 +0100

    Create a TravelDeals model

--- app/src/main/java/com/zakariyaf/travelmantics/InsertActivity.java
+++ app/src/main/java/com/zakariyaf/travelmantics/InsertActivity.java
@@ -6,12 +6,21 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class InsertActivity extends AppCompatActivity {
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mFirebaseDatabase.getReference().child("traveldeals");
     }
 
     @Override
