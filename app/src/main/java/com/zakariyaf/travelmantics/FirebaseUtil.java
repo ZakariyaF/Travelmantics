90bf5edb6eb0568b72373ff97a9f2bd877826adf
ZakariyaF <zak.ferhi@gmail.com>
2019-08-02 14:19:46 +0100

    Fill the FirebaseUtil class

--- app/src/main/java/com/zakariyaf/travelmantics/FirebaseUtil.java
+++ app/src/main/java/com/zakariyaf/travelmantics/FirebaseUtil.java
@@ -1,6 +1,26 @@
 package com.zakariyaf.travelmantics;
 
 
-public class FirebaseUtil {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+
+public class FirebaseUtil {
+    public static FirebaseDatabase sFirebaseDatabase;
+    public static DatabaseReference sDatabaseReference;
+    public static ArrayList<TravelDeal> sTravelDeals;
+    private static FirebaseUtil sFirebaseUtil;
+
+    private FirebaseUtil() {
+    }
+
+    public static void openFirebaseReference(String ref) {
+        if (sFirebaseUtil == null) {
+            sFirebaseUtil = new FirebaseUtil();
+            sFirebaseDatabase = FirebaseDatabase.getInstance();
+            sTravelDeals = new ArrayList<>();
+        }
+        sDatabaseReference = sFirebaseDatabase.getReference().child(ref);
+    }
 }
