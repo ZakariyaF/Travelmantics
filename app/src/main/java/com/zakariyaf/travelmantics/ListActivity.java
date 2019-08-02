0a2d4f3152c249b4ec89ecce955cb474ac7b35a6
ZakariyaF <zak.ferhi@gmail.com>
2019-08-02 12:26:07 +0100

    Implement the ListActivity to display deals titles in the TextView

--- app/src/main/java/com/zakariyaf/travelmantics/ListActivity.java
+++ app/src/main/java/com/zakariyaf/travelmantics/ListActivity.java
@@ -1,14 +1,62 @@
 package com.zakariyaf.travelmantics;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity {
+
+    private ArrayList<TravelDeal> mTravelDeals;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+    private ChildEventListener mChildListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mFirebaseDatabase.getReference().child("traveldeals");
+        mChildListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                TextView tvDeals = findViewById(R.id.tvDeals);
+                TravelDeal travelDeal = dataSnapshot.getValue(TravelDeal.class);
+                tvDeals.setText(tvDeals.getText() + "\n" + travelDeal.getTitle());
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        mDatabaseReference.addChildEventListener(mChildListener);
     }
 }
