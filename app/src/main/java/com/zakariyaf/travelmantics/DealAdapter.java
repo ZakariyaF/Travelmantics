93ed3b27643ce233e7e566989dc36cf1b09dbc2f
ZakariyaF <zak.ferhi@gmail.com>
2019-08-02 15:37:13 +0100

    Update the DealAdapter with the new added TextViews

--- app/src/main/java/com/zakariyaf/travelmantics/DealAdapter.java
+++ app/src/main/java/com/zakariyaf/travelmantics/DealAdapter.java
@@ -87,14 +87,20 @@ public class DealAdapter extends RecyclerView.Adapter<DealAdapter.DealViewHolder
 
     public class DealViewHolder extends RecyclerView.ViewHolder {
         TextView tvTitle;
+        TextView tvDescription;
+        TextView tvPrice;
 
         public DealViewHolder(@NonNull View itemView) {
             super(itemView);
             tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvDescription = itemView.findViewById(R.id.tvDescription);
+            tvPrice = itemView.findViewById(R.id.tvPrice);
         }
 
         public void bind(TravelDeal travelDeal) {
             tvTitle.setText(travelDeal.getTitle());
+            tvDescription.setText(travelDeal.getDescription());
+            tvPrice.setText(travelDeal.getPrice());
         }
     }
 }
