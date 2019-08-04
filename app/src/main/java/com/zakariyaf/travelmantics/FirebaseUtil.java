5b39f393d721d7e0fef5ede6dc24042f3bdebe40
ZakariyaF <zak.ferhi@gmail.com>
2019-08-04 15:26:51 +0100

    Handle signing in

--- app/src/main/java/com/zakariyaf/travelmantics/FirebaseUtil.java
+++ app/src/main/java/com/zakariyaf/travelmantics/FirebaseUtil.java
@@ -1,15 +1,22 @@
 package com.zakariyaf.travelmantics;
 
 
+import android.app.Activity;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 
+import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class FirebaseUtil {
+
     public static FirebaseDatabase sFirebaseDatabase;
     public static DatabaseReference sDatabaseReference;
     public static FirebaseAuth sFirebaseAuth;
@@ -17,23 +24,45 @@ public class FirebaseUtil {
     public static ArrayList<TravelDeal> sTravelDeals;
     private static FirebaseUtil sFirebaseUtil;
 
+    private static final int RC_SIGN_IN = 123;
+    private static Activity mCallerActivity;
+
     private FirebaseUtil() {
     }
 
-    public static void openFirebaseReference(String ref) {
+    public static void openFirebaseReference(String ref, final Activity callerActivity) {
         if (sFirebaseUtil == null) {
             sFirebaseUtil = new FirebaseUtil();
             sFirebaseDatabase = FirebaseDatabase.getInstance();
             sFirebaseAuth = FirebaseAuth.getInstance();
+            mCallerActivity = callerActivity;
             sAuthListener = new FirebaseAuth.AuthStateListener() {
                 @Override
                 public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-
+                    FirebaseUtil.signIn();
+                    Toast.makeText
+                            (mCallerActivity.getBaseContext(), "Welcome back!", Toast.LENGTH_LONG).show();
                 }
             };
         }
         sTravelDeals = new ArrayList<>();
         sDatabaseReference = sFirebaseDatabase.getReference().child(ref);
+    }
+
+    private static void signIn() {
+        Activity callerActivity = ;
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+
+// Create and launch sign-in intent
+        mCallerActivity.startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
     }
 
     public static void attachListener() {
