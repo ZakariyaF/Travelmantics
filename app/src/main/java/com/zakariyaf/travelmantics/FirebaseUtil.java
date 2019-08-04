57a7c2c8c78fff8ad42a778a5b652828861fac22
ZakariyaF <zak.ferhi@gmail.com>
2019-08-04 16:07:34 +0100

    Update onAuthStateChanged callback to sign in only logged users

--- app/src/main/java/com/zakariyaf/travelmantics/FirebaseUtil.java
+++ app/src/main/java/com/zakariyaf/travelmantics/FirebaseUtil.java
@@ -39,7 +39,9 @@ public class FirebaseUtil {
             sAuthListener = new FirebaseAuth.AuthStateListener() {
                 @Override
                 public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                    FirebaseUtil.signIn();
+                    if (firebaseAuth.getCurrentUser() == null) {
+                        FirebaseUtil.signIn();
+                    }
                     Toast.makeText
                             (mCallerActivity.getBaseContext(), "Welcome back!", Toast.LENGTH_LONG).show();
                 }
