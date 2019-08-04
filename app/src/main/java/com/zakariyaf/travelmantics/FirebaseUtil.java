83437f4fb0ead055754a757ea19b52f5a2260a47
ZakariyaF <zak.ferhi@gmail.com>
2019-08-04 14:33:15 +0100

    Setup the FirebaseAuth object and necessary methods

--- app/src/main/java/com/zakariyaf/travelmantics/FirebaseUtil.java
+++ app/src/main/java/com/zakariyaf/travelmantics/FirebaseUtil.java
@@ -1,6 +1,9 @@
 package com.zakariyaf.travelmantics;
 
 
+import androidx.annotation.NonNull;
+
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -9,6 +12,8 @@ import java.util.ArrayList;
 public class FirebaseUtil {
     public static FirebaseDatabase sFirebaseDatabase;
     public static DatabaseReference sDatabaseReference;
+    public static FirebaseAuth sFirebaseAuth;
+    public static FirebaseAuth.AuthStateListener sAuthListener;
     public static ArrayList<TravelDeal> sTravelDeals;
     private static FirebaseUtil sFirebaseUtil;
 
@@ -19,8 +24,24 @@ public class FirebaseUtil {
         if (sFirebaseUtil == null) {
             sFirebaseUtil = new FirebaseUtil();
             sFirebaseDatabase = FirebaseDatabase.getInstance();
+            sFirebaseAuth = FirebaseAuth.getInstance();
+            sAuthListener = new FirebaseAuth.AuthStateListener() {
+                @Override
+                public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                }
+            };
         }
         sTravelDeals = new ArrayList<>();
         sDatabaseReference = sFirebaseDatabase.getReference().child(ref);
     }
+
+    public static void attachListener() {
+        sFirebaseAuth.addAuthStateListener(sAuthListener);
+    }
+
+    public static void detachListener() {
+        sFirebaseAuth.removeAuthStateListener(sAuthListener);
+    }
+
 }
