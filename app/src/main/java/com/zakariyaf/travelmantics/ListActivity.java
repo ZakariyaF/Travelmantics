8a702021d95bd1cbd1e6780360a52cc84caedba4
ZakariyaF <zak.ferhi@gmail.com>
2019-08-04 14:37:25 +0100

    Adapt the AuthStateListener with the Activity's lifecycle

--- app/src/main/java/com/zakariyaf/travelmantics/ListActivity.java
+++ app/src/main/java/com/zakariyaf/travelmantics/ListActivity.java
@@ -51,4 +51,15 @@ public class ListActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        FirebaseUtil.detachListener();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        FirebaseUtil.attachListener();
+    }
 }
